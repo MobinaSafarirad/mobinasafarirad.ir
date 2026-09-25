aa7f7df08bf53661b7244525283d95dfb4470dd1
MobinaSafarirad <mobinasafarirad@gmail.com>
2026-09-25 16:27:54 +0330

    edited .gitignore

--- lib/locale.ts
+++
@@ -1,9 +0,0 @@
-import { hasLocale } from "next-intl";
-import { routing, type Locale } from "@/i18n/routing";
-
-export function asLocale(value: string): Locale {
-  if (hasLocale(routing.locales, value)) {
-    return value;
-  }
-  return routing.defaultLocale;
-}
