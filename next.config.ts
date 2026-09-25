053d1a10a7036eab3b0dc6f002fb85e6b6e0643d
MobinaSafarirad <mobinasafarirad@gmail.com>
2026-09-25 17:48:22 +0330

    minor change

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: "/mobinasafarirad.ir",
+  assetPrefix: "/mobinasafarirad.ir",
   images: {
     unoptimized: true,
   },
